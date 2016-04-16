0e7ad9c3e55de4232e418ae43ba745d9dbc9af70
guardian4711 <gwiepert@gmx.de>
2016-04-16 16:59:48 +0000

    Irgendwie mag ich Tabellen

--- workproject/mdrza/forms.py
+++ workproject/mdrza/forms.py
@@ -4,18 +4,52 @@ from .models import radler, woche, team
 
 class WochenForm(forms.ModelForm):
 
-    class Meta:
-        model = woche
-        fields = ('kwnr', 'mo', 'di', 'mi', 'do', 'fr',
-			'mo_extra', 'di_extra', 'mi_extra', 'do_extra', 'fr_extra')
+	class Meta:
+		model = woche
+		fields = (
+			'kwnr',
+			'mo',
+			'mo_extra',
+			'di',
+			'di_extra',
+			'mi',
+			'mi_extra',
+			'do',
+			'do_extra',
+			'fr',
+			'fr_extra'
+		)
+		labels = {
+			'kwnr': ('Kalenderwoche'),
+			'mo': ('Montag'),
+			'di': ('Dienstag'),
+			'mi': ('Mittwoch'),
+			'do': ('Donnerstag'),
+			'fr': ('Freitag'),
+			'mo_extra': ('Extra'),
+			'di_extra': ('Extra'),
+			'mi_extra': ('Extra'),
+			'do_extra': ('Extra'),
+			'fr_extra': ('Extra'),
+		}
 
 
 class RadlerForm(forms.ModelForm):
 	class Meta:
 		model = radler
 		fields = ('vname', 'nname', 'email', 'team', 'km')
+		labels = {
+			'vname': ('Vorname'),
+			'nname': ('Nachname'),
+			'email': ('E-Mail'),
+			'team': ('Team'),
+			'km': ('Tages-KM'),
+		}
 
 class TeamForm(forms.ModelForm):
 	class Meta:
 		model = team
 		fields = ( 'name', )
+		labels = {
+			'name': ('Team'),
+		}
