e73198330007208592e06a7f65d2fe71cfdcc4c1
netzdienste <nd@ofd-h.de>
2016-04-20 14:13:34 +0200

    wsgi für Prod angepasst

--- workproject/workproject/wsgi.py
+++ workproject/workproject/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+import sys
 import os
 
-from django.core.wsgi import get_wsgi_application
-
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/..')
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../workproject')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workproject.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
