14d52e8a8f1e046de63fd8f6aac1559d4ae0ac8d
netzdienste <nd@ofd-h.de>
2016-04-20 14:11:43 +0200

    Pfad für account/login unter mdrza/ verschoben

--- workproject/mdrza/views.py
+++ workproject/mdrza/views.py
@@ -25,7 +25,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            return HttpResponseRedirect("/accounts/login/")
+            return HttpResponseRedirect("/mdrza/accounts/login/")
     else:
         form = UserCreationForm()
     return render(request, "registration/register.html", {
